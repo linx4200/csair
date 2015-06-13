8f8af72e74e0df2c1c90b33bf6f9649988b11cc1
liuxr <liuxr@ucweb.com>
2015-06-13 15:33:09 +0800

    DONE

--- csair.rb
+++ csair.rb
@@ -3,6 +3,31 @@
 require 'mechanize'  #gem install mechanize
 require 'mail' #gem install mail
 
+class Qmail
+  def initialize(account, password)
+    # 需要在 QQ邮箱 开启SMTP 服务
+    Mail.defaults do
+      delivery_method :smtp, { :address              => 'smtp.qq.com',
+                               :port                 => 25,
+                               :domain               => 'localhost',
+                               :user_name            => account,
+                               :password             => password,
+                               :authentication       => :login,
+                               :enable_starttls_auto => false  }
+    end
+    @account = account
+  end
+  def send(to, subject, msg)
+    account =  @account
+    Mail.deliver do
+      to to
+      from account
+      subject subject
+      body msg
+    end
+  end
+end
+
 def fetch()
   agent = Mechanize.new
   url = 'http://b2c.csair.com/B2C40/query/jaxb/interDirect/query.ao'
@@ -20,19 +45,31 @@ end
 
 def main()
   old_price = 0
+  mail = Qmail.new('youremail@qq.com', 'password')
+  flag = false
 
   while(1)
     res = fetch
     if res[:err]
-      # todo
+      flag = true
+      msg = '拿不回来数据了! 说不定就被封了! 老公自己去看看呀!'
+    else
+      price = res['segment'][0]['dateflight'][0]['prices'][0]['adultprice'].to_s
+      offset = price - old_price
+
+      if offset < 0
+        flag = true
+        msg = '机票跳水' + (-offset).to_s  + 'RMB啦!!!! 现价: ' + price.to_s + ' 大甩卖!'
+      else
+        flag = true
+        msg = '竟然涨价了!!!!涨了' + offset.to_s + 'RMB这么贵!!!! 现在都要' + price.to_s + ' 那么贵了! 😢'
+      end
     end
 
-    price = res['segment'][0]['dateflight'][0]['prices'][0]['adultprice']
-
-    if price < old_price
-      # todo
+    if flag
+      mail.send('241923064@qq.com', '湾湾机票呼喊老公哒~~~', msg)
     end
-
+    flag = false
     old_price = price
 
     sleep(60 * 5)
